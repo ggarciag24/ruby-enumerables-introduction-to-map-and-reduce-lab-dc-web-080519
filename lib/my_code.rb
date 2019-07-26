8dc7c34fe616ebbcf1af50fc72ff50bffeeb176c
German Garcia <ggarciag.business@outlook.com>
2019-07-26 15:05:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# My Code here....
+
+def map_to_negativize(sourceArray)
+   sourceArray.map() {|i| i * -1}
+end
+
+def map_to_no_change(sourceArray)
+    return sourceArray
+end
+
+def map_to_double(sourceArray)
+    sourceArray.map() {|i| i * 2}
+end
+
+def map_to_square(sourceArray)
+    sourceArray.map() {|i| i * i}
+end
+
+def reduce_to_total(sourceArray, startingPoint = 0)
+    sourceArray.reduce(startingPoint) { |total, num| total + num}
+end
+
+def reduce_to_all_true(sourceArray) 
+    sourceArray.reduce(true, :&)
+end
+
+def reduce_to_any_true(sourceArray)
+    sourceArray.reduce(initial, :|)
+end
