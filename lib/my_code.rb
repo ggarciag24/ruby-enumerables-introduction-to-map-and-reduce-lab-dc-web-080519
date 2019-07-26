79e3d9d431290b6f2a03cdb49c35c01613a7256f
German Garcia <ggarciag.business@outlook.com>
2019-07-26 15:56:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,7 +27,7 @@ def reduce_to_all_true(source_array)
     #check if its a truthy or falsy value 
     #if a truthy value is present return true if not then return false 
     
-    if n == true 
+    if  
   end
 end
 
